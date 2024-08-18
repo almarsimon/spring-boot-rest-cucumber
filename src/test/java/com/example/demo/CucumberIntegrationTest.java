866c09c8df72974f1aedf0af0837a6ccbe268d84
JJ <jjlind000@gmail.com>
2024-08-18 22:56:01 +0100

    Add summary plugin

--- src/test/java/com/example/demo/CucumberIntegrationTest.java
+++ src/test/java/com/example/demo/CucumberIntegrationTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty"},
+        plugin = {"pretty","summary"},
         features = "classpath:features/",
         glue = {"com.example.demo.cucumber"}
 )
